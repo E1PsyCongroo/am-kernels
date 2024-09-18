6bd2ec755a901972ff503dd214aad714192fe9d9
focused_xy <elpsycongroo242@gmail.com>
2024-09-18 14:29:34 +0800

    feat(soc-tests): add psram mem-test

--- tests/soc-tests/tests/mem-test.c
+++ tests/soc-tests/tests/mem-test.c
@@ -1,48 +1,103 @@
 #include "trap.h"
 
-int main() {
-  uint8_t *p1 = heap.start;
-  for (int i = 0; i < 64; i++) {
+void sram_test(void) {
+  void* sram_start = (void*)0x0f000000;
+  void* sram_end   = (void*)0x0f002000;
+  uint8_t *p1 = sram_start;
+  while (p1 != sram_end) {
     *p1 = (uintptr_t)p1 & 0xff;
     p1++;
   }
-  p1 = heap.start;
-  for (int i = 0; i < 64; i++) {
+  p1 = sram_start;
+  while (p1 != sram_end) {
     check(*p1 == ((uintptr_t)p1 & 0xff));
     p1++;
   }
 
-  uint16_t *p2 = heap.start;
-  for (int i = 0; i < 64; i++) {
+  uint16_t *p2 = sram_start;
+  while (p2 != sram_end) {
     *p2 = (uintptr_t)p2 & 0xffff;
     p2++;
   }
-  p2 = heap.start;
-  for (int i = 0; i < 64; i++) {
+  p2 = sram_start;
+  while (p2 != sram_end) {
     check(*p2 == ((uintptr_t)p2 & 0xffff));
     p2++;
   }
 
-  uint32_t *p3 = heap.start;
-  for (int i = 0; i < 64; i++) {
+  uint32_t *p3 = sram_start;
+  while (p3 != sram_end) {
     *p3 = (uintptr_t)p3 & 0xffffffff;
     p3++;
   }
-  p3 = heap.start;
-  for (int i = 0; i < 64; i++) {
+  p3 = sram_start;
+  while (p3 != sram_end) {
     check(*p3 == ((uintptr_t)p3 & 0xffffffff));
     p3++;
   }
 
-  uint64_t *p4 = heap.start;
-  for (int i = 0; i < 64; i++) {
+  uint64_t *p4 = sram_start;
+  while (p4 != sram_end) {
     *p4 = (uint64_t)(uintptr_t)p3 & 0xffffffffffffffff;
     p4++;
   }
-  p4 = heap.start;
-  for (int i = 0; i < 64; i++) {
+  p4 = sram_start;
+  while (p4 != sram_end) {
     check(*p4 == ((uint64_t)(uintptr_t)p3 & 0xffffffffffffffff));
     p4++;
   }
+}
+
+void psram_test(void) {
+  void* psram_start = (void*)0x80000000;
+  void* psram_end   = (void*)0x80400000;
+  uint8_t *p1 = psram_start;
+  while (p1 != psram_end) {
+    *p1 = (uintptr_t)p1 & 0xff;
+    p1++;
+  }
+  p1 = psram_start;
+  while (p1 != psram_end) {
+    check(*p1 == ((uintptr_t)p1 & 0xff));
+    p1++;
+  }
+
+  uint16_t *p2 = psram_start;
+  while (p2 != psram_end) {
+    *p2 = (uintptr_t)p2 & 0xffff;
+    p2++;
+  }
+  p2 = psram_start;
+  while (p2 != psram_end) {
+    check(*p2 == ((uintptr_t)p2 & 0xffff));
+    p2++;
+  }
+
+  uint32_t *p3 = psram_start;
+  while (p3 != psram_end) {
+    *p3 = (uintptr_t)p3 & 0xffffffff;
+    p3++;
+  }
+  p3 = psram_start;
+  while (p3 != psram_end) {
+    check(*p3 == ((uintptr_t)p3 & 0xffffffff));
+    p3++;
+  }
+
+  uint64_t *p4 = psram_start;
+  while (p4 != psram_end) {
+    *p4 = (uint64_t)(uintptr_t)p3 & 0xffffffffffffffff;
+    p4++;
+  }
+  p4 = psram_start;
+  while (p4 != psram_end) {
+    check(*p4 == ((uint64_t)(uintptr_t)p3 & 0xffffffffffffffff));
+    p4++;
+  }
+}
+
+int main() {
+  sram_test();
+  psram_test();
   return 0;
 }
